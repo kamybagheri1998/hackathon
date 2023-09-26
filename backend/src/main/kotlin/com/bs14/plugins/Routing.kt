e1c372e7410c1c1fcc7f9c62a2dfc93d79b521d4
Jan Leon Greve <janleon.greve@scoop-software.de>
2023-09-26 12:51:43 +0200

    Added backend

---
+++ backend/src/main/kotlin/com/bs14/plugins/Routing.kt
@@ -0,0 +1,20 @@
+package com.bs14.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.autohead.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    install(AutoHeadResponse)
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        // Static plugin. Try to access `/static/index.html`
+        static("/static") {
+            resources("static")
+        }
+    }
+}
---
+++ backend/src/test/kotlin/com/bs14/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.bs14
+
+import com.bs14.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
