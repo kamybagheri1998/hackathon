5720454eeb9157bff7d2de977f308dc1b25f7d0c
Jan Leon Greve <janleon.greve@scoop-software.de>
2023-09-27 09:45:24 +0200

    Inspector warnings

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,9 +1,9 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val versionKtor: String by project
+val versionKotlin: String by project
+val versionLogback: String by project
+val versionExposed: String by project
+val versionH2: String by project
 
-val exposed_version: String by project
-val h2_version: String by project
 plugins {
     kotlin("jvm") version "1.9.10"
     id("io.ktor.plugin") version "2.3.4"
@@ -25,6 +25,8 @@ repositories {
 }
 
 dependencies {
+    implementation("ch.qos.logback:logback-classic:$versionLogback")
+
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-auth-jvm")
     implementation("io.ktor:ktor-server-sessions-jvm")
@@ -33,11 +35,12 @@ dependencies {
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("org.jetbrains.exposed:exposed-core:$versionExposed")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$versionExposed")
+    implementation("com.h2database:h2:$versionH2")
+
     testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$versionKotlin")
 }
