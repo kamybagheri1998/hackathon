8756edbe856632ed2f2704b79715d03efdd24049
maxstaas1910 <maximilianleon.staas@otto.de>
2023-09-28 12:34:55 +0200

    added more basic structure for frontend

---
+++ frontend/scripts/loginAndSignup.js
@@ -0,0 +1,34 @@
+document.getElementById("signInForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("signInEmail").value;
+  const password = document.getElementById("signInPassword").value;
+
+  const credentials = {
+    username: email,
+    password: password
+  };
+  //TODO add post api url
+  fetch('', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.success) {
+      // TODO maybe a modal?
+      // TODO add login token
+      // TODO add correct url
+      window.location.replace("http://localhost:8080/");
+    } else {
+      window.location.replace("https://www.google.com");
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    window.location.replace("error.html");
+  });
+});
