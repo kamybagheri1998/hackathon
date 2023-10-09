e17e18cdbe97e886cdb384221dc59d3ec659af34
Jan Leon Greve <janleon.greve@scoop-software.de>
2023-10-09 11:29:12 +0200

    Added test logging

--- backend/src/main/kotlin/com/bs14/plugins/Databases.kt
+++ backend/src/main/kotlin/com/bs14/plugins/Databases.kt
@@ -5,6 +5,7 @@ import DeviceService
 import LoanService
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -13,6 +14,8 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.jetbrains.exposed.sql.*
 import java.util.*
+
+private val LOG = KotlinLogging.logger {}
 
 fun Application.configureDatabases() {
     val database = Database.connect(
@@ -29,15 +32,29 @@ fun Application.configureDatabases() {
 
     routing {
         get("/user") {
-            val email = call.request.queryParameters["email"]!!
-            val passwordHash = call.request.queryParameters["password"]!!.hashCode()
+            LOG.info { "Test 1" }
+            val email: String = call.request.queryParameters["email"]!!
+            LOG.info { "Test 2" }
+            val password: String = call.request.queryParameters["password"]!!
+            LOG.info { "Test 3" }
+            val passwordHash: Int = password.hashCode()
+            LOG.info { "Test 4" }
 
             if (!userService.login(email, passwordHash)) {
                 call.respond(HttpStatusCode.Unauthorized)
                 return@get
             }
 
-            call.respond(HttpStatusCode.OK)
+            LOG.info { "Test 5" }
+
+            val token = JWT.create()
+                .withAudience(jwtAudience)
+                .withIssuer(jwtAudience)
+                .withClaim("username", email)
+                .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                .sign(Algorithm.HMAC256(jwtSecret))
+
+            call.respond(HttpStatusCode.OK, hashMapOf("token" to token))
         }
 
         post("/user") {
@@ -51,14 +68,7 @@ fun Application.configureDatabases() {
             val password = call.request.queryParameters["password"]!!
             val id = userService.create(email, institute, password)
 
-            val token = JWT.create()
-                .withAudience(jwtAudience)
-                .withIssuer(jwtAudience)
-                .withClaim("username", email)
-                .withExpiresAt(Date(System.currentTimeMillis() + 60000))
-                .sign(Algorithm.HMAC256(jwtSecret))
-
-            call.respond(HttpStatusCode.Created, hashMapOf("token" to token, "userId" to id))
+            call.respond(HttpStatusCode.Created, hashMapOf("userId" to id))
         }
 
         authenticate {
